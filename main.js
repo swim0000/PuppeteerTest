d70524a8d4f4beaa7c85e86a6d99273e367b83f7
james <jamesparker0000@gmail.com>
2023-12-03 17:24:57 +0000

    last

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const csvFile = 'urls.csv'; 
-const outputFolder = './html'; 
+const csvFile = 'urls.csv';
+const outputFolder = './html/';
 
 (async () => {
   const browser = await puppeteer.launch();
+
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
   const page = await browser.newPage();
 
   const csvData = fs.readFileSync(csvFile, 'utf8');
   const urls = csvData.split('\n').map(line => line.trim());
 
   for (const url of urls) {
-    console.log(`Scraping URL: ${url}`);
-    await page.goto(url);
+    try {
+      console.log(`Scraping URL: ${url}`);
+      await page.goto(url);
 
-    const content = await page.content();
+      const content = await page.content();
 
-    const fileName = `${url.split('/').pop()}.html`;
-    fs.writeFileSync(fileName, content);
+      const fileName = `${url.split('/').pop()}.html`;
+      fs.writeFileSync(`${outputFolder}/${fileName}`, content);
 
-    await page.waitForTimeout(1000);
+      await page.waitForTimeout(100);
+    } catch (error) {
+      console.error(`Error scraping URL ${url}: ${error.message}`);
+    }
   }
 
   await browser.close();
