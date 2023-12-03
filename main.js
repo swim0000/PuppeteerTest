6f82958237c077397acb6e3c2f0835301057be1f
james <jamesparker0000@gmail.com>
2023-12-03 16:41:45 +0000

    added csv and filepath of saved htmls

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const csvFile = 'urls.csv'; 
+const outputFolder = './scraped_pages'; 
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://www.example.com');
- 
-await page.click('button[type="button"]'); 
-  await page.waitForSelector('input[type="file"]'); 
+  const csvData = fs.readFileSync(csvFile, 'utf8');
+  const urls = csvData.split('\n').map(line => line.trim());
 
-  const input = await page.$('input[type="file"]');
-  await input.uploadFile('example.html'); 
+  const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const csvFile = 'urls.csv'; 
+const outputFolder = './scraped_pages'; 
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const csvData = fs.readFileSync(csvFile, 'utf8');
+  const urls = csvData.split('\n').map(line => line.trim());
+
+  for (const url of urls) {
+    console.log(`Scraping URL: ${url}`);
+    await page.goto(url);
+
+    const content = await page.content();
+
+    const fileName = `${C:\htmls\farming}/${url.split('/').pop()}.html`;
+    fs.writeFileSync(fileName, content, 'utf8');
+
+    await page.waitForTimeout(1000); 
+  }
 
   await browser.close();
 })();
