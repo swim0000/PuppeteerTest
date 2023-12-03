ad6939868c02bcc2526f86d7faeff52f0f709140
james <jamesparker0000@gmail.com>
2023-12-03 16:56:50 +0000

    puppet install

--- main.js
+++ main.js
@@ -2,20 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const csvFile = 'urls.csv'; 
-const outputFolder = './scraped_pages'; 
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  const csvData = fs.readFileSync(csvFile, 'utf8');
-  const urls = csvData.split('\n').map(line => line.trim());
-
-  const puppeteer = require('puppeteer');
-const fs = require('fs');
-
-const csvFile = 'urls.csv'; 
-const outputFolder = './scraped_pages'; 
+const outputFolder = './html'; 
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -30,10 +17,10 @@ const outputFolder = './scraped_pages';
 
     const content = await page.content();
 
-    const fileName = `${C:\htmls\farming}/${url.split('/').pop()}.html`;
-    fs.writeFileSync(fileName, content, 'utf8');
+    const fileName = `${url.split('/').pop()}.html`;
+    fs.writeFileSync(fileName, content);
 
-    await page.waitForTimeout(1000); 
+    await page.waitForTimeout(1000);
   }
 
   await browser.close();
